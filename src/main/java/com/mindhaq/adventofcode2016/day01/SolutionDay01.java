e2547967f925c8fb89dee600057feda60e01a3ea
Rüdiger Schulz <rueschu@gmail.com>
2016-12-05 11:31:45 +0100

    day02: better naming to prevent 25 classes with the same name

--- src/main/java/com/mindhaq/adventofcode2016/day01/SolutionDay01.java
+++ src/main/java/com/mindhaq/adventofcode2016/day01/SolutionDay01.java
@@ -10,13 +10,13 @@ import static com.google.common.base.Charsets.UTF_8;
 import static com.google.common.io.Resources.getResource;
 import static org.slf4j.LoggerFactory.getLogger;
 
-public class Solution {
+public class SolutionDay01 {
 
 	public static void main(String... args) throws IOException {
-		final Logger logger = getLogger(Solution.class);
+		final Logger logger = getLogger(SolutionDay01.class);
 
 		TaxiCabWalker taxiCabWalker = new TaxiCabWalker();
-		String input = Resources.toString(getResource(Solution.class, "input.txt"), UTF_8);
+		String input = Resources.toString(getResource(SolutionDay01.class, "input.txt"), UTF_8);
 
 		for (String instruction: Splitter.on(", ").trimResults().split(input)) {
 			taxiCabWalker.walk(instruction);
