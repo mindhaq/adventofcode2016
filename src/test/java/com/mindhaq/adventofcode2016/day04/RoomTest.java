c8295f020b8951a4e385fbc05538dbf9e6cb548d
Rüdiger Schulz <rueschu@gmail.com>
2017-10-21 23:49:58 +0200

    Day04: add tests for all examples in the puzzle description

--- src/test/java/com/mindhaq/adventofcode2016/day04/RoomTest.java
+++ src/test/java/com/mindhaq/adventofcode2016/day04/RoomTest.java
@@ -6,7 +6,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class RoomTest {
 
-    private final static String EXAMPLE1 = "aaaaa-bbb-z-y-x-123[abxyz]";
+    static final String EXAMPLE1 = "aaaaa-bbb-z-y-x-123[abxyz]";
+    static final String EXAMPLE2 = "a-b-c-d-e-f-g-h-987[abcde]";
+    static final String EXAMPLE3 = "not-a-real-room-404[oarel]";
+    static final String EXAMPLE4 = "totally-real-room-200[decoy]";
 
     @Test
     public void reads_name() {
@@ -33,6 +36,36 @@ public class RoomTest {
         Room room = Room.fromCode(EXAMPLE1);
 
         assertThat(room.isReal()).isTrue();
+    }
+
+    @Test
+    public void it_parses_second_example() {
+        Room room = Room.fromCode(EXAMPLE2);
+
+        assertThat(room.getName()).isEqualTo("a-b-c-d-e-f-g-h");
+        assertThat(room.getSectorId()).isEqualTo(987);
+        assertThat(room.getChecksum()).isEqualTo("abcde");
+        assertThat(room.isReal()).isTrue();
+    }
+
+    @Test
+    public void it_parses_third_example() {
+        Room room = Room.fromCode(EXAMPLE3);
+
+        assertThat(room.getName()).isEqualTo("not-a-real-room");
+        assertThat(room.getSectorId()).isEqualTo(404);
+        assertThat(room.getChecksum()).isEqualTo("oarel");
+        assertThat(room.isReal()).isTrue();
+    }
+
+    @Test
+    public void it_parses_fourth_example() {
+        Room room = Room.fromCode(EXAMPLE4);
+
+        assertThat(room.getName()).isEqualTo("totally-real-room");
+        assertThat(room.getSectorId()).isEqualTo(200);
+        assertThat(room.getChecksum()).isEqualTo("decoy");
+        assertThat(room.isReal()).isFalse();
     }
 
 }
