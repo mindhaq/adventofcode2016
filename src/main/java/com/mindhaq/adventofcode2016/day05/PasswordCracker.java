616b7734217a4f767d1b9136106cd6de8cbc91f6
Rüdiger Schulz <rueschu@gmail.com>
2018-06-14 12:28:26 +0200

    Day05: Part2: different approach using only stream API

--- src/main/java/com/mindhaq/adventofcode2016/day05/PasswordCracker.java
+++ src/main/java/com/mindhaq/adventofcode2016/day05/PasswordCracker.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import java.util.stream.IntStream;
 
 import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toMap;
 
 /**
  * @author Rüdiger Schulz &lt;rs@mindhaq.com&gt;
@@ -49,11 +50,39 @@ public class PasswordCracker {
             count++;
         }
 
-        return password
+        String crack1 = password
                 .entrySet()
                 .stream()
                 .sorted(Comparator.comparingInt(Map.Entry::getKey))
                 .map(Map.Entry::getValue)
                 .collect(joining());
+
+        log.info("Crack1: {}", crack1);
+
+        // different approach using only stream API. not perfect, see below.
+        String crack2 = IntStream
+                .range(0, Integer.MAX_VALUE)
+                .boxed()
+                .sequential()
+                .map(i -> doorId + i)
+                .map(Hash::new)
+                .filter(Hash::isInterestingPosition)
+                .peek(hash -> log.info("Found {}", hash))
+                .limit(20)      // this needs to be large enough to find at least 8 unique tokens!
+                .collect(toMap(
+                        Hash::getPosition,
+                        Hash::getTokenAtPosition,
+                        (t1, t2) -> t1
+                ))
+                .entrySet()
+                .stream()
+                .sorted(Comparator.comparingInt(Map.Entry::getKey))
+                .limit(8)
+                .map(Map.Entry::getValue)
+                .collect(joining());
+
+        log.info("Crack2: {}", crack2);
+
+        return crack1;
     }
 }
