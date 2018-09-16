adba7841f6577ac085a9b8c4882a0cdfd70fe7df
Rüdiger Schulz <rueschu@gmail.com>
2018-09-16 17:08:33 +0200

    Day08: parse rect

--- src/main/java/com/mindhaq/adventofcode2016/day08/OperationParser.java
+++ src/main/java/com/mindhaq/adventofcode2016/day08/OperationParser.java
@@ -1,8 +1,23 @@
 package com.mindhaq.adventofcode2016.day08;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class OperationParser {
 
+    private final static Pattern PARSE_REC = Pattern.compile("rect (\\d+)x(\\d+)");
+
     public static Operation parse(String commandText) {
-        throw new IllegalArgumentException("cannot parse " + commandText);
+        Matcher matcher;
+
+        matcher = PARSE_REC.matcher(commandText);
+        if (matcher.matches()) {
+            var width = Integer.parseInt(matcher.group(1));
+            var height = Integer.parseInt(matcher.group(2));
+
+            return new Rect(width, height);
+        }
+
+        throw new IllegalArgumentException("cannot parse '" + commandText + "'");
     }
 }
--- src/test/java/com/mindhaq/adventofcode2016/day08/OperationParserTest.java
+++ src/test/java/com/mindhaq/adventofcode2016/day08/OperationParserTest.java
@@ -2,10 +2,28 @@ package com.mindhaq.adventofcode2016.day08;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class OperationParserTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void throws_for_unparsable_text() {
         OperationParser.parse("gibberish");
+    }
+
+    @Test
+    public void parses_rect_operation() {
+        // given
+        var command = "rect 3x2";
+
+        // when
+        Operation operation = OperationParser.parse(command);
+
+        // then
+        assertThat(operation).isNotNull();
+
+        var rect = (Rect) operation;
+        assertThat(rect.getWidth()).isEqualTo(3);
+        assertThat(rect.getHeight()).isEqualTo(2);
     }
 }
